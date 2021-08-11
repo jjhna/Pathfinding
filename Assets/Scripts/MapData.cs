cd72a188f63f938ecc1f477631c37e4aed22e05e
Jon <22411356+jjhna@users.noreply.github.com>
2021-08-11 13:19:54 -0400

    Constructing the maze
    Constructing the maze in Unity.
    
    Found methods to build in the map of the maze by two different methods:
    1. By using a text file with 1's and 0's.
    2. By using Texture Map from an image psd file (note in the future you need to edit the import settings).
    Also note that the MapData.cs file takes the text file also from the Resources->Mapdata folder area for the text file that has the same name as the current scene (ie. demo.txt).

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MapData : MonoBehaviour
+{
+    public int width = 10;
+    public int height = 5;
+
+    public TextAsset textAsset;
+    public Texture2D textureMap;
+    public string resourcePath = "Mapdata";
+
+    void Awake()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+
+        if (textureMap == null)
+        {
+            textureMap = Resources.Load(resourcePath + "/" + levelName) as Texture2D;
+        }
+
+        if (textAsset == null)
+        {
+
+            textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
+        }
+    }
+
+
+    public List<string> GetMapFromTextFile(TextAsset tAsset)
+    {
+        List<string> lines = new List<string>();
+
+        if (tAsset != null)
+        {
+            string textData = tAsset.text;
+            string[] delimiters = { "\r\n", "\n" };
+            lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
+            lines.Reverse();
+        }
+        else
+        {
+            Debug.LogWarning("MAPDATA GetTextFromFile Error: invalid TextAsset");
+        }
+        return lines;
+
+    }
+
+
+    public List<string> GetMapFromTextFile()
+    {
+        return GetMapFromTextFile(textAsset);
+    }
+
+    public List<string> GetMapFromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+
+        if (texture != null)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                string newLine = "";
+
+                for (int x = 0; x < texture.width; x++)
+                {
+                    if (texture.GetPixel(x, y) == Color.black)
+                    {
+                        newLine += '1';
+                    }
+                    else if (texture.GetPixel(x, y) == Color.white)
+                    {
+                        newLine += '0';
+                    }
+                    else
+                    {
+                        newLine += ' ';
+                    }
+                }
+                lines.Add(newLine);
+            }
+        }
+
+        return lines;
+    }
+
+    public void SetDimensions(List<string> textLines)
+    {
+        height = textLines.Count;
+
+        foreach (string line in textLines)
+        {
+            if (line.Length > width)
+            {
+                width = line.Length;
+            }
+        }
+    }
+
+    public int[,] MakeMap()
+    {
+        List<string> lines = new List<string>();
+
+        if (textureMap != null)
+        {
+            lines = GetMapFromTexture(textureMap);
+        }
+        else
+        {
+            lines = GetMapFromTextFile(textAsset);
+        }
+
+        SetDimensions(lines);
+
+        int[,] map = new int[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (lines[y].Length > x)
+                {
+                    map[x, y] = (int)Char.GetNumericValue(lines[y][x]);
+                }
+            }
+        }
+        return map;
+
+    }
+}
